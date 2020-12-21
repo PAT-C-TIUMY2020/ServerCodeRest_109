e9ec1af273189d2e3fb5e7e9078ebbf02ffb2fad
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-12-21 14:15:54 +0700

    Add project files.

---
+++ ServerCodeRest_109_Tsaqif/Program.cs
@@ -0,0 +1,51 @@
+﻿using ServerCodeRest_109_Tsaqif.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using ServiceRest_109_Tsaqif;
+using System.Text;
+using System.Threading.Tasks;
+using ITI_UMY = ServiceRest_109_Tsaqif.ITI_UMY;
+
+namespace ServerCodeRest_109_Tsaqif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:8733/Design_Time_Addresses/ServiceRest_109_Tsaqif/Service1/");
+            WebHttpBinding binding = new WebHttpBinding();
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), binding, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                hostObjek.Description.Behaviors.Add(smb);
+                Binding mexbinding = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbinding, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("Server ready...");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
